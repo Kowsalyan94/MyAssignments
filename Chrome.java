dc168e869575bb04cd117f694e1c8696b564747e
Kowsalyan94 <nkowsalya.94@gmail.com>
2025-05-29 10:26:58 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+		System.out.println("Version of Chorme Browser is " + version);
+		
+		String developer = "Google";
+		System.out.println("Developer Name is " + developer);
+		
+		boolean isBeta = false;
+		System.out.println( "This a Beta version is "+ isBeta);
+		
+		int releaseYear = 2008;
+		System.out.println("Release Year is " + releaseYear);
+
+		char shortCut = 'C';
+		System.out.println("Short cut key for launching Chorme is " + shortCut);
+		
+	}
+
+}
